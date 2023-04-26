8c7f734c01d6584131e5890c899c47caf4277966
Frederik <frederiktaekwondo@outlook.com>
2023-04-26 12:07:01 +0200

    BulletScript nearly done & PlayerShooter

---
+++ Assets/_Scripts/PlayerShooter.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+using UnityEngine;
+
+public class PlayerShooter : MonoBehaviour
+{
+    public PlayerShooter laserprefab;
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Mouse0))
+        {
+            Shoot();
+        }
+    }
+    private void Shoot()
+    {
+        Instantiate(this.laserprefab, this.transform.position, Quaternion.identity);
+    }
+}
